7c072435fae75356b0806474c8e39e9766da86eb
Esther Kang <eskang87@gmail.com>
2021-12-16 13:00:53 -0800

    commit tree 7a4129384a53a34db79a8fc234c86af5cb275687

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
@@ -15,3 +17,26 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+
+/*
+//FETCH LAB- finding 
+//1. The 5th book in the series
+//2. The 1031st character in the series
+//3. The total number of pages of all the books
+fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => {
+        console.log(json);
+        console.log(`The 5th book in the series: ${json[4].name}`);
+//The 1031st character in the series
+        console.log(json[0].characters);
+
+        let totalPages = 0;
+        json.forEach(objElem => {
+            totalPages += objElem.numberOfPages;
+        });
+        console.log(`The total number of pages of all the books: ${totalPages}`);
+});
+*/
